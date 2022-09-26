fe0a6225f8d460ae8e42ddb74b868c1b144640d8
adamf0 <adamilkom00@gmail.com>
2022-09-26 16:15:07 +0700

    perbaikan view kampus/mahasiswa/show

--- resources/views/kampus/mahasiswa/show.blade.php
+++ resources/views/kampus/mahasiswa/show.blade.php
@@ -19,90 +19,94 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-6">
-    <div class="card">
-        <div class="card-header">Rencana Pembayaran Mahasiswa</div>
-        <div class="card-body">
-            <div class="overflow-auto">
-                <table class="table table-responsive table-bordered text-center align-middle">
-                    <thead>
-                        <tr>
-                            <th>Tanggal</th>
-                            <th>Pembayaran</th>
-                            <th>Semester</th>
-                            <th>Biaya</th>
-                            <th>Keterangan</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @php $i=0; @endphp
-                        @foreach ($bulanans as $key_group => $item_group)
-                            @foreach ($item_group as $index => $rencana)
-                            @php
-                                $date1 = strtotime($key_group);
-                                $date2 = strtotime(date('Y-m-d'));
-                                $diff = $date1-$date2;
-                            @endphp
+    <div class="col-md-7">
+        <div class="card">
+            <div class="card-header">Rencana Pembayaran Mahasiswa</div>
+            <div class="card-body">
+                <div class="overflow-auto">
+                    <table class="table table-responsive table-bordered text-center align-middle">
+                        <thead>
                             <tr>
-                                @if (count($item_group)>=1 && $index==0)
-                                    <td rowspan="{{ count($item_group) }}">{{ \Carbon\Carbon::parse($rencana->tanggal_bayar)->format('j F Y') }}</td>
-                                @endif                            
-                                <td>{{ $rencana->item_bayar->item->nama }}</td>
-                                <td>{{ "Rp ".number_format($rencana->biaya, 0, ",", ".") }}</td>
-                                <td>{{ $rencana->nama }}</td>
-                                <td>Semester {{ floor($i/$max_cicilan)+1 }}</td>
-                                <td>
-                                    @if ($rencana->status==0 && $diff==0)
-                                    <label class="badge bg-warning">Menunggu Pembayaran</label>
-                                    @elseif ($rencana->status==0 && $diff < 0)
-                                    <label class="badge bg-danger">Belum Bayar</label>
-                                    @elseif ($rencana->status==0 && $diff > 0)
-                                    <label class="badge bg-secondary">Belum Ada Tagihan</label>
-                                    @elseif ($rencana->status==1)
-                                    <label class="badge bg-success">Sudah Bayar</label>
-                                    @endif
-                                </td>
+                                <th>Tanggal</th>
+                                <th>Pembayaran</th>
+                                <th>Semester</th>
+                                <th>Biaya</th>
+                                <th>Keterangan</th>
+                                <th>Status</th>
                             </tr>
+                        </thead>
+                        <tbody>
+                            @php $i=0; @endphp
+                            @foreach ($bulanans as $key_group => $item_group)
+                                @foreach ($item_group as $index => $rencana)
+                                @php
+                                    $date1 = strtotime($key_group);
+                                    $date2 = strtotime(date('Y-m-d'));
+                                    $diff = $date1-$date2;
+                                @endphp
+                                <tr>
+                                    @if (count($item_group)>=1 && $index==0)
+                                        <td rowspan="{{ count($item_group) }}">{{ \Carbon\Carbon::parse($rencana->tanggal_bayar)->format('j F Y') }}</td>
+                                    @endif                            
+                                    <td>{{ $rencana->item_bayar->item->nama }}</td>
+                                    <td>{{ "Rp ".number_format($rencana->biaya, 0, ",", ".") }}</td>
+                                    <td>{{ $rencana->nama }}</td>
+                                    <td>Semester {{ floor($i/$max_cicilan)+1 }}</td>
+                                    <td>
+                                        @if ($rencana->status==0 && $diff==0)
+                                        <label class="badge bg-warning">Menunggu Pembayaran</label>
+                                        @elseif ($rencana->status==0 && $diff < 0)
+                                        <label class="badge bg-danger">Belum Bayar</label>
+                                        @elseif ($rencana->status==0 && $diff > 0)
+                                        <label class="badge bg-secondary">Belum Ada Tagihan</label>
+                                        @elseif ($rencana->status==1)
+                                        <label class="badge bg-success">Sudah Bayar</label>
+                                        @endif
+                                    </td>
+                                </tr>
+                                @endforeach
+                                @php $i++ @endphp
                             @endforeach
-                            @php $i++ @endphp
-                        @endforeach
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-    <div class="col-md-6">
+    <div class="col-md-5">
         <div class="card">
             <div class="card-header">Pembayaran Non-Bulanan</div>
             <div class="card-body">
-                <table class="table table-responsive table-bordered text-center align-middle" id="nonbulanan">
-                    <thead>
-                        <tr>
-                            <th>Pembayaran</th>
-                            <th>Biaya</th>
-                            <th>Keterangan</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($non_bulanans as $index => $item)
+                <div class="overflow-auto">
+                    <table class="table table-responsive table-bordered text-center align-middle">
+                        <thead>
                             <tr>
-                                <td>{{ $item->item_bayar->item->nama }}</td>
-                                <td>{{ "Rp ".number_format($item->biaya, 0, ",", ".") }}</td>
-                                <td>{{ $item->nama }}</td>
-                                <td>
-                                    @if ($item->status==0)
-                                    <label class="badge bg-danger">Belum Bayar</label>
-                                    @else
-                                    <label class="badge bg-success">Sudah Bayar</label>
-                                    @endif
-                                </td>
+                                <th>Pembayaran</th>
+                                <th>Biaya</th>
+                                <th>Keterangan</th>
+                                <th>Status</th>
                             </tr>
-                            @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($non_bulanans as $index => $item)
+                                <tr>
+                                    <td>{{ $item->item_bayar->item->nama }}</td>
+                                    <td>{{ "Rp ".number_format($item->biaya, 0, ",", ".") }}</td>
+                                    <td>{{ $item->nama }}</td>
+                                    <td>
+                                        @if ($item->status==0)
+                                        <label class="badge bg-danger">Belum Bayar</label>
+                                        @else
+                                        <label class="badge bg-success">Sudah Bayar</label>
+                                        @endif
+                                    </td>
+                                </tr>
+                                @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
